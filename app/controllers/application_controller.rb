5cef12a10dbada15f8b119ba0f0da71c0ac5e9af
NGUYEN NGOC TRUNG <nguyen.ngoc.trung@framgia.com>
2017-09-05 09:28:49 +0700

    refs task#N/A test action pack variant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :detect_browser
+
+  private
+  def mobile?
+    iphone = request.user_agent =~ /iPhone/
+    ipod_touch = request.user_agent =~ /iPod touch/
+    ipad = request.user_agent =~ /iPad/
+    android = request.user_agent =~ /Android/
+    android_tablet = request.user_agent =~ Regexp.union(/(?!Android.+Mobile)Android/, /Android.+SC-01C/)
+    (iphone || ipod_touch || android) && !ipad && !android_tablet
+  end
+
+  def tablet?
+    ipad = request.user_agent =~ /iPad/
+    android_tablet = request.user_agent =~ Regexp.union(/(?!Android.+Mobile)Android/, /Android.+SC-01C/)
+    ipad || android_tablet
+  end
+
+  def detect_browser
+    request.variant = case true
+                      when mobile?
+                        :mobile
+                      when tablet?
+                        :tablet
+                      else
+                        :desktop
+                      end
+  end
 end
