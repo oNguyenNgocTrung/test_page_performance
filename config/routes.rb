485bc0301ff8ef770580401457283a81d26dc6f7
NGUYEN NGOC TRUNG <nguyen.ngoc.trung@framgia.com>
2017-10-02 09:20:12 +0700

    refs task#N/A error handling in rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   end
   resources :test_variants, only: [:index]
   root to: "images#index"
+
+  match '/404', to: 'error/errors#not_found', via: :all
+  match '/500', to: 'error/errors#internal_server_error', via: :all
 end
